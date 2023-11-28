effc6b2075bf2bb200dd6f3abf49b308706adddb
Aurelien MEUNIER <aurelien.meunier.59860@outlook.fr>
2023-11-28 13:12:27 +0100

    Ajout avis pour livre

---
+++ js/swipe.js
@@ -0,0 +1,27 @@
+console.log("test");
+
+new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    autoplay: {
+        delay: 5000,
+      },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
