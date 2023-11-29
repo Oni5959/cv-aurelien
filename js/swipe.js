a6c6590512d2431af3777b081f464f1d39858a4d
Aurelien MEUNIER <aurelien.meunier.59860@outlook.fr>
2023-11-29 16:42:31 +0100

    résolution  du défilement des avis

--- js/swipe.js
+++ js/swipe.js
@@ -1,27 +1,48 @@
 console.log("test");
 
-new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    autoplay: {
-        delay: 5000,
-      },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
   
-    // And if we need scrollbar
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  // autoplay: {
+  //   delay: 5000,
+  // },
+ 
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
+
   });
-  
+
+
